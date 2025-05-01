a4d802870b58a73e08b25e70eef936e9f5afc1e6
Serhii Krasnozhon <zhonik92@gmail.com>
2025-05-01 23:51:50 +0300

    create homework

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client';
 import 'modern-normalize';
 import './index.css';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
